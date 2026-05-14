6fbbe461e4afdd3b705779d3cd688444a7ab91b7
wesamjoma <wesam.joma.92@gmail.com>
2026-05-14 19:11:23 +0200

    Fix setup_auth.py

--- setup_auth.py
+++ setup_auth.py
@@ -2,7 +2,7 @@ from playwright.sync_api import sync_playwright
 
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
+    browser = p.chromium.launch(headless=True)
     context = browser.new_context()
     page = context.new_page()
     
