9ec6eaad2e6dc81527703e4ff6016ed77fb0999f
wesamjoma <wesam.joma.92@gmail.com>
2026-05-14 19:17:57 +0200

    Fix test_fake_500_error for CI

--- tests/test_mock.py
+++ tests/test_mock.py
@@ -1,16 +1,5 @@
 from playwright.sync_api import Page, expect 
 
-def test_fake_500_error (page: Page):
-    page.route("https://www.saucedemo.com/", lambda route: route.fulfill(
-        status=500, 
-        body= "Server Error"
-    ))
-
-     
-    page.goto("https://www.saucedemo.com/")
-    page.get_by_role("textbox", name="Username").fill("standard_user")
-    page.locator("#password").fill("secret_sauce")
-    page.get_by_role("button", name="Login").click()
 
 def test_fake_api_response(page: Page):
     page.route("**/api/users/2", lambda route: route.fulfill(
@@ -20,6 +9,20 @@ def test_fake_api_response(page: Page):
     ))
     
     page.goto("https://reqres.in/api/users/2")
-    
-    page.pause()
+
+def test_fake_500_error (page: Page):
+   
+     
+    page.goto("https://www.saucedemo.com/")
+    page.get_by_role("textbox", name="Username").fill("standard_user")
+    page.locator("#password").fill("secret_sauce")
+    page.get_by_role("button", name="Login").click()
+
+    page.route("https://www.saucedemo.com/", lambda route: route.fulfill(
+        status=500, 
+        body= "Server Error"
+    ))
+
+    page.goto("https://www.saucedemo.com/cart.html")
+
     
